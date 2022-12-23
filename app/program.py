e8d33a04b851c0f55567c54dbfd87a3a047244ae
plstars <62638010+plstars@users.noreply.github.com>
2022-12-23 05:38:02 +0000

    .

--- app/program.py
+++ app/program.py
@@ -1,34 +1,29 @@
 from pacman import play, move_ghosts
 from ui import ui_print, ui_key, ui_msg_lost, ui_msg_win
+from app.utilities import common as pll
 
-# @ -> our hero
-# G -> ghosts
-# P -> pills
-# . -> empty spaces
-# | and - -> walls
-map = [
-    "|--------|",
-    "|G..|..G.|",
-    "|...PP...|",
-    "|G...@.|.|",
-    "|........|",
-    "|--------|"
-]
+map = pll.map
 
-game_finished = False
+if not pll.check_map(map):
+    print("Invalid map")
 
-while not game_finished:
-    ui_print(map)
-    key = ui_key()
-    valid_key, pacman_alive, won = play(map, key)
+else:
+    print("Valid map")
 
-    pacman_was_hit = move_ghosts(map)
+    game_finished = False
 
-    if (not pacman_alive) or (pacman_was_hit):
-        ui_msg_lost()
-        game_finished = True
-    elif won:
-        ui_msg_win()
-        game_finished = True
+    while not game_finished:
+        ui_print(map)
+        key = ui_key()
+        valid_key, pacman_alive, won = play(map, key)
+
+        pacman_was_hit = move_ghosts(map)
+
+        if (not pacman_alive) or (pacman_was_hit):
+            ui_msg_lost()
+            game_finished = True
+        elif won:
+            ui_msg_win()
+            game_finished = True
 
 # End of file
