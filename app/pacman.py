ac3d7623f2ec2e3579c89a5d67329ca617ae4717
plstars <62638010+plstars@users.noreply.github.com>
2022-12-23 04:48:45 +0000

    .

--- app/pacman.py
+++ app/pacman.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from datetime import datetime
 
 from app.utilities import common as pl
 # from app.utilities import locale as pll
@@ -9,7 +8,5 @@ print("Hello World")
 
 print(f'Environment is {pl.get_environment()}')
 
-# print(f'Locale is {pll.get_locale()}')
-print(pl.onFutureDay(datetime.today(), 0))
 
 # End
