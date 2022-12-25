880966d01e439d9838a9c74068ec8b17ba363111
plstars <62638010+plstars@users.noreply.github.com>
2022-12-25 05:33:21 +0000

    .

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # pac2
 
 Maze game
- 
+
+
+Using PGZero
+
+
+https://pygame-zero.readthedocs.io/en/stable/
+
--- app/pac_test/test_pytest.py
+++ app/pac_test/test_pytest.py
@@ -1,10 +1,3 @@
-# import pytest
-from app.utilities import common as pl  # NoQA E402
-
-
-def test_locale():
-    assert pl.show_testing() == "TEST_DATA"  # "Should be TEST_DATA"
-
 
 def test_sum():
     assert sum([1, 2, 3]) == 6  # "Should be 6"
