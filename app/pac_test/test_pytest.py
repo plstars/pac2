45275b7ca3515a481988db5ba0812eb4e1c9d7b2
plstars <62638010+plstars@users.noreply.github.com>
2022-12-25 05:14:30 +0000

    .

--- app/pac_test/test_pytest.py
+++ app/pac_test/test_pytest.py
@@ -1,9 +1,3 @@
-# import pytest
-<<<<<<< HEAD
-=======
-from app.utilities import common as pll  # NoQA E402
->>>>>>> e8d33a04b851c0f55567c54dbfd87a3a047244ae
-
 
 def test_sum():
     assert sum([1, 2, 3]) == 6  # "Should be 6"
@@ -12,10 +6,4 @@ def test_sum():
 def test_sum_tuple():
     assert sum((1, 2, 2)) == 5  # "Should be 5"
 
-
-<<<<<<< HEAD
-=======
-def test_valid_map():
-    assert pll.check_map(pll.map) is True  # "Should be True"
->>>>>>> e8d33a04b851c0f55567c54dbfd87a3a047244ae
 # End
